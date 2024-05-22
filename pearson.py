0dec9836e6eb705765d9a970828c0113d5ef1efa
Melina Moraiti <melinamoraith@gmail.com>
2024-05-22 23:21:50 +0300

    Pearson from multiple fav input vectors

--- pearson.py
+++ pearson.py
@@ -1,18 +1,12 @@
 import numpy as np
+from scipy.stats import pearsonr
 
-
-def pearson_correlation(vector, matrix):
-    # Subtract the mean of each column
-    matrix_centered = matrix - np.mean(matrix, axis=0)
-    vector_centered = vector - np.mean(vector)
-    
-    # Compute the numerator (covariance)
-    numerator = np.sum(matrix_centered * vector_centered, axis=1)
-    
-    # Compute the denominator (product of standard deviations)
-    denominator = np.sqrt(np.sum(matrix_centered**2, axis=1)) * np.sqrt(np.sum(vector_centered**2))
-    
-    # Avoid division by zero
-    denominator[denominator == 0] = 1
-    
-    return numerator / denominator
+def pearson_correlation(favorite_vector, all_vectors):
+    correlations = []
+    for vector in all_vectors:
+        if np.array_equal(favorite_vector, vector):
+            correlations.append(0)  # to avoid perfect correlation with itself
+        else:
+            correlation, _ = pearsonr(favorite_vector, vector)
+            correlations.append(correlation)
+    return np.array(correlations)
