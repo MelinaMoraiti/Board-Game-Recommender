9dcdceedf9bd00e307193107b9342062ef735c54
Melina Moraiti <melinamoraith@gmail.com>
2024-06-17 18:40:31 +0300

    Create seperate preprocessing scripts for old and new dataset

---
+++ preprocess_csv.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def load_and_merge_data():
+    games = pd.read_csv('datasets/games.csv')
+    mechanics = pd.read_csv('datasets/mechanics.csv')
+    themes = pd.read_csv('datasets/themes.csv')
+    subcategories = pd.read_csv('datasets/subcategories.csv')
+
+    # Define the categories to extract from games dataframe
+    categories = ['Cat:Thematic', 'Cat:War', 'Cat:Strategy', 'Cat:Family', 'Cat:CGS', 'Cat:Abstract', 'Cat:Party', 'Cat:Childrens']
+    categories_df = games[['BGGId', 'Name', 'AvgRating'] + categories]
+
+    # Merge datasets on BGGId
+    boardgames_df = categories_df.merge(mechanics, on='BGGId', how='left')\
+                                 .merge(themes, on='BGGId', how='left')\
+                                 .merge(subcategories, on='BGGId', how='left')
+
+    return boardgames_df
+
+def save_boardgames_df(df, path='datasets/boardgames_df.csv'):
+    df.to_csv(path, index=False)
+
+if __name__ == "__main__":
+    boardgames_df = load_and_merge_data()
+    save_boardgames_df(boardgames_df)
